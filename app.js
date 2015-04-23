c960dac24614068308b9d80c12d83dc6fde7a1cf
Brian Kropff & Kyle Giard-Chase <student@epicodus.com>
2015-04-23 14:18:34 -0700

    Create view to create player.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var darkMenance = angular.module("darkMenance", ["ui.router"]);
 
-darkMenance.config(function($stateProvider) {
-  $stateProvider.state("home", {
-    url: "",
-    templateUrl: "partials/start.html"
-  });
+  darkMenance.config(function($stateProvider) {
+
+    $stateProvider.state("home", {
+      url: "",
+      templateUrl: "partials/start.html"
+    });
+
+    $stateProvider.state("create_char", {
+      url: "create",
+      templateUrl: "partials/create_char.html",
+      controller: "PlayersCtrl"
+    });
 });
